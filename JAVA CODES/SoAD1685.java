c727274130e9b4362b00c7b13cfe011467468b93
SnhsDs <snehashis187@gmail.com>
2025-10-30 10:37:48 +0530

    Revert "My first upload of java codes"
    This reverts commit 6fc32b68702d926850f27f34510f6eb394244aca.

--- JAVA CODES/SoAD1685.java
+++
@@ -1,6 +0,0 @@
-// LeetCode Problem 1685: Sum of Absolute Differences in a Sorted Array
-// Link: https://leetcode.com/problems/sum-of-absolute-differences-in-a-sorted-array/
-
-public class SoAD1685 {
-
-}
